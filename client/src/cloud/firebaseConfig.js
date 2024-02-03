99d4ce00b3100a1d0e46846dfe084bcfb7376725
AnshulRai21 <112742861+AnshulRai21@users.noreply.github.com>
2024-02-04 00:23:00 +0530

    Update firebaseConfig.js

--- client/src/cloud/firebaseConfig.js
+++ client/src/cloud/firebaseConfig.js
@@ -4,12 +4,6 @@ import {getStorage} from "firebase/storage"
 // Replace with your Firebase project's configuration
 const firebaseConfig = {
     apiKey: "AIzaSyCiPSt65zrgiCaDO2XvUcCtzHtK0kzKNLU",
-    authDomain: "gitamlostnfound.firebaseapp.com",
-    projectId: "gitamlostnfound",
-    storageBucket: "gitamlostnfound.appspot.com",
-    messagingSenderId: "725308759760",
-    appId: "1:725308759760:web:4c951d861010d4468e08d5"
- 
 };
 
 // Initialize Firebase
